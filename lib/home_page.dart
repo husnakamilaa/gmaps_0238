a89361834c248c79f963604ab2405008d950ff81
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-05-08 10:41:16 +0700

    feat: deklarasi statefull di home page dan menyusun content didalamnya dalam row center

---
+++ lib/home_page.dart
@@ -0,0 +1,45 @@
+import 'dart:ui_web';
+
+import 'package:flutter/material.dart';
+import 'package:gmaps/map_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? alamatDipilih;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
